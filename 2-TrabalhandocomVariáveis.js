b92749fb4b28354e17f6f24d6b13129c03118ca4
isabelamendesgavloski <167210673+isabelamendesgavloski@users.noreply.github.com>
2024-09-12 08:47:51 -0300

    Add files via upload

--- 2-TrabalhandocomVariáveis.js
+++ 2-TrabalhandocomVariáveis.js
@@ -4,5 +4,8 @@ console.log("Trabalhando com variáveis");
 
 const idade = 29;
 const nome = "Ricardo";
-let ano = 2020;
+
+//não fazer
+//mes = "Janeiro";
+
 
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com atribuição de variáveis");
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0;
+contador = +1;
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável;
+idade = 26; //atribuindo valor;
+idade = idade+1;
+console.log(idade);
+
+
