6450ab3aeb86d759de54e671a3ef79e134814d15
isabelamendesgavloski <167210673+isabelamendesgavloski@users.noreply.github.com>
2024-09-19 08:40:09 -0300

    Add files via upload

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,16 +5,30 @@ const listaDeDestinos = new Array(
     'Rio de Janeiro'
 );
 
-const idadeComprador = 21;
+const idadeComprador = 17;
+const estaAcompanhada = true;
+
+
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 
 if(idadeComprador >= 18){
     console.log("Comprador maior de idade");
     listaDeDestinos.splice(1,1); // removendo item
-}
+}else if(estaAcompanhada == true){
+        console.log("Comprador está acompanhado");
+        listaDeDestinos.splice(1,1); //removendo item
 
+    }else{
+        console.log("Não é maior de Idade e não posso vender");
 
+    }
 
 
 console.log(listaDeDestinos);
+
+//console.log(idadeComprador > 18);
+console.log(idadeComprador < 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador == 18);
